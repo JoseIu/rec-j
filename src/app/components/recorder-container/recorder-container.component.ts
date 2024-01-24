411659814556731c2f535ff5cc08f0d05fbda60c
Joselu <joseluissgz97@gmail.com>
2024-01-24 13:26:44 +0100

    refactoring and adding preview

--- src/app/components/recorder-container/recorder-container.component.ts
+++ src/app/components/recorder-container/recorder-container.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RecorderButtonComponent } from '../recorder-button/recorder-button.component';
 
 @Component({
@@ -10,35 +10,68 @@ import { RecorderButtonComponent } from '../recorder-button/recorder-button.comp
   styleUrl: './recorder-container.component.scss',
 })
 export class RecorderContainerComponent {
-  mediarecorder: MediaRecorder | undefined;
+  @ViewChild('preview') public preview?: ElementRef;
+  @ViewChild('download') public download?: ElementRef;
+
+  private mediarecorder: MediaRecorder | undefined;
   public isRecording: boolean = false;
 
+  public existURL: boolean = false;
+
+  public constraints = {
+    audio: true,
+    // video: true,
+  };
+
   public async recording() {
-    const media = await navigator.mediaDevices.getDisplayMedia({
-      audio: true,
-      video: { frameRate: { ideal: 30 } },
-    });
-    this.mediarecorder = new MediaRecorder(media, {
-      mimeType: 'video/webm;codecs=vp8,opus',
-      audioBitsPerSecond: 128000,
-    });
-    this.mediarecorder.start();
-    this.isRecording = true;
+    try {
+      // Obetenemos los dispotistivos de entrada audio y video
+      const userMedia = await navigator.mediaDevices.getUserMedia(
+        this.constraints
+      );
 
-    const [video] = media.getVideoTracks();
-    video.addEventListener('ended', () => {
-      if (this.mediarecorder) {
-        this.mediarecorder.stop();
-      }
-    });
+      //Obtenemos la ventana a compartir
+      const media = await navigator.mediaDevices.getDisplayMedia({
+        audio: true,
+        video: { frameRate: { ideal: 30 } },
+      });
 
-    this.mediarecorder.addEventListener('dataavailable', (e) => {
-      const link = document.createElement('a');
-      link.href = URL.createObjectURL(e.data);
-      link.download = 'captura.webm';
-      link.click();
-    });
-    this.isRecording = false;
+      //Combinamos los dos streams audio y ventana
+      const tracks = [...media.getTracks(), ...userMedia.getTracks()];
+
+      const combinedStream = new MediaStream(tracks);
+
+      this.mediarecorder = new MediaRecorder(combinedStream, {
+        mimeType: 'video/webm;codecs=vp8,opus',
+        audioBitsPerSecond: 128000,
+      });
+      this.mediarecorder.start();
+      this.isRecording = true;
+
+      const [video] = media.getVideoTracks();
+      video.addEventListener('ended', () => {
+        if (this.mediarecorder) {
+          this.mediarecorder.stop();
+          this.isRecording = false;
+        }
+      });
+
+      this.mediarecorder.addEventListener('dataavailable', (e) => {
+        const url = URL.createObjectURL(e.data);
+
+        if (this.preview) {
+          this.preview.nativeElement.src = url;
+          this.preview.nativeElement.play();
+        }
+        if (this.download) {
+          this.download.nativeElement.href = url;
+        }
+      });
+      this.isRecording = false;
+      this.existURL = true;
+    } catch (error) {
+      console.log('Error: ', error);
+    }
   }
 
   public stopRecording() {
